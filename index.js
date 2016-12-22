70339f54d13bce6a461f4cb9092ac21394515a64
BrOrlandi <brorlandi@gmail.com>
2016-12-22 00:27:52 -0200

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var koa = require('koa');
+var koaBody = require('koa-body');
+
+var app = koa();
+
+app.use(koaBody({formidable:{uploadDir: __dirname}}));
+app.use(function *(){
+    console.log("Method: ",this.request.method);
+    console.log("Query:\n", this.request.query,"\n");
+    console.log("Body:\n",this.request.body);
+});
+
+app.listen(3000);
+console.log("Listening port 3000");
